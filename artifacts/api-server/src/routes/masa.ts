05ad58bc9df57a92b2c7788e897931523864d6bc
usecomus-afk <usecomus@gmail.com>
2026-04-19 11:14:06 +0300

    fix(gunesin): update google review link to tvnoLJvxZcTGn60vK

--- artifacts/api-server/src/routes/masa.ts
+++ artifacts/api-server/src/routes/masa.ts
@@ -2263,7 +2263,7 @@ textarea.fb-form-field{resize:vertical;min-height:80px}
   </div>
   <div class="fb-high" id="fbHigh">
     <p class="fb-high-msg">Harika! Google'da değerlendirme yapmak ister misiniz?</p>
-    <a class="fb-gmaps-btn" href="https://share.google/cam4YI0t26psrgOFA" target="_blank" rel="noopener noreferrer" id="fbGmapsBtn">
+    <a class="fb-gmaps-btn" href="https://share.google/tvnoLJvxZcTGn60vK" target="_blank" rel="noopener noreferrer" id="fbGmapsBtn">
       <svg width="18" height="18" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z" fill="#fff"/></svg>
       Google'da Değerlendir
     </a>
